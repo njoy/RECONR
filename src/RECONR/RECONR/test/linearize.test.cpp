55f205c53eea8b8a78f8e49af9765d5f182620f8
Jeremy Conlin <jlconlin@lanl.gov>
2020-03-03 15:02:22 -0500

    Trying to verify the linearization.

--- src/RECONR/RECONR/test/linearize.test.cpp
+++ src/RECONR/RECONR/test/linearize.test.cpp
@@ -12,25 +12,34 @@ SCENARIO( "Testing the linearization of collected cross sections" ){
     auto r2d2 = njoy::RECONR::R2D2::Factory( std::move( material ) )();
 
     THEN( "the cross sections can be linearized" ){
-      auto linear = njoy::RECONR::RECONR::linearize( r2d2, 1E-6, 1E-4 );
+      auto linear = njoy::RECONR::RECONR::linearize( r2d2, 1E-6, 1E-3 );
 
       auto rXSs = r2d2.crossSections();
       auto lXSs = linear.crossSections();
 
       auto keys = ranges::view::keys( rXSs );
       CHECK( ranges::equal( keys, ranges::view::keys( lXSs ) ) );
-
-      {
-        int MT{ 1 };
+      
+      for( auto MT: keys ){
+        njoy::Log::info( "MT: {}", MT );
         auto rxs = rXSs.at( MT );
         auto lxs = lXSs.at( MT );
 
         auto refX = rxs.x() | ranges::to_vector;
+        auto refY = rxs.y() | ranges::to_vector;
         auto linX = lxs.x() | ranges::to_vector;
-        njoy::Log::info( "refX: {}", refX | ranges::view::all );
-        njoy::Log::info( "linX: {}", linX | ranges::view::all );
-        CHECK( ranges::equal( refX, linX ) );
+        auto linY = lxs.y() | ranges::to_vector;
+        auto oY = lxs.x() | ranges::view::transform( rxs ) | ranges::to_vector;
+        // njoy::Log::info( "{}, refX: {}", MT, refX | ranges::view::all );
+        // njoy::Log::info( "{}, refY: {}", MT, refY | ranges::view::all );
+        // njoy::Log::info( "{}, linX: {}", MT, linX | ranges::view::all );
+        // njoy::Log::info( "{}, linY: {}", MT, linY | ranges::view::all );
+        // njoy::Log::info( "{}, oY: {}", MT, oY | ranges::view::all );
+        // CHECK( ranges::equal( refX, linX ) );
       }
+
+
+
     } // THEN
   } // GIVEN
 } // SCENARIO
