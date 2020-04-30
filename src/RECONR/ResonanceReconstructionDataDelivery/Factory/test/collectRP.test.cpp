91494dff0c1fda75f913d584605fdc2f6b7b70f8
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2020-04-30 10:53:42 -0600

    Can collect resonance parameters from ENDF.

---
+++ src/RECONR/ResonanceReconstructionDataDelivery/Factory/test/collectRP.test.cpp
@@ -0,0 +1,25 @@
+#include "catch.hpp"
+
+#include "RECONR.hpp"
+
+#include "RECONR/details/simpleENDFTestString.hpp"
+
+SCENARIO( "Testing the collection of resonance parameter data" ){
+
+  GIVEN( "ENDF Material with SLBW" ){
+    auto material = ENDFMaterial( "SLBW" );
+
+    WHEN( "the resonance parameters are extracted" ){
+      auto isos = std::get< 0 >(
+        njoy::RECONR::R2D2::Factory::collectRP( material ) );
+
+      THEN( "we can verify a few things" ){
+        // Note, I'm not testing everything because I trust ENDFtk to do it's
+        // job
+        CHECK( 1 == ranges::distance( isos ) );
+      } // THEN
+    } // WHEN
+    
+  } // GIVEN
+  
+} // SCENARIO
