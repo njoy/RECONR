8185084fd7214baa69cf85ac64a10ebe3f33a70b
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2020-05-21 07:25:22 -0600

    Playing some tricks with MT=18.

--- src/RECONR/RECONR/src/summateReactions.hpp
+++ src/RECONR/RECONR/src/summateReactions.hpp
@@ -39,6 +39,8 @@ auto summateReactions( ResonanceReconstructionDataDelivery& r2d2,
   };
 
   // Reconstruct everything
+  // Perhaps this could be more efficient by only reconstructing those things
+  // that are used, but I doubt that is the limiting factor here
   for( const auto& [ MT, XS ] : linear ){
     
     reactions[ MT ] =  energies 
@@ -47,11 +49,25 @@ auto summateReactions( ResonanceReconstructionDataDelivery& r2d2,
   }
 
   // Add reconstructed resonances
-  for( const auto& [ MT, recon ] : reconstructed ){
+  for( const auto& MT : ranges::view::keys( reconstructed ) ){
+    using PType = decltype( reconstructed )::mapped_type;
+    std::unique_ptr< PType > recon = nullptr;
+
+    // Add resonance data to MT=19 if there are partials
+    if( MT == 18 ){
+      if( reactions.find( 19 ) != reactions.end() ){
+        recon = std::make_unique< PType >( reconstructed.at( 19 ) );
+      }
+    }
+    if( recon == nullptr ){
+      recon = std::make_unique< PType >( reconstructed.at( MT ) );
+    }
+
+
     std::vector< std::vector< double >  > partials{ reactions.at( MT ) };
 
     // Get cross sections from reconstructed reactions
-    for( const auto& XS : recon ){
+    for( const auto& XS : *recon ){
       auto partial = energies | ranges::view::transform( XS );
       partials |= ranges::action::push_back( partial );
     }
@@ -72,11 +88,14 @@ auto summateReactions( ResonanceReconstructionDataDelivery& r2d2,
       
       std::vector< std::vector< double > > partials;
       for( const auto& p : redundants ){
+
         auto found = reactions.find( p );
 
+        // Look for already reconstructed reactions
         if( found != reactions.end() ){
           partials |= ranges::action::push_back( found->second );
         } else{
+          // Calculate the summed reaction since it doesn't already exist
           auto foundReaction = linear.find( p );
           if( foundReaction != linear.end() ){
             auto part = foundReaction->second;
@@ -86,7 +105,6 @@ auto summateReactions( ResonanceReconstructionDataDelivery& r2d2,
             partials |= ranges::action::push_back( party );
           }
         }
-
         reactions[ MT ] = sumPartials( partials );
       }
     }
