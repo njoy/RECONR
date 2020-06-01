4113cce0cda0aed64abbe49b73570e355bf05c7d
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2020-06-01 17:01:20 -0600

    Getting rid of SIGABRT.
    Yahoo

--- src/RECONR/RECONR/src/getEvaluated.hpp
+++ src/RECONR/RECONR/src/getEvaluated.hpp
@@ -4,9 +4,7 @@ Format_t getEvaluated( int nendf ){
   std::string filename = "tape" + std::to_string( nendf );
   try {
     auto evaluated = njoy::utility::slurpFileToMemory( filename );
-    Tape_t tape = njoy::ENDFtk::syntaxTree::makeTape( 
-        std::move( evaluated ) );
-    return tape;
+    return Format_t{ std::move( evaluated ) };
   } catch( ... ){
     Log::info( "Error ocurred when trying to open evaluated data file: {}.",
                filename );
