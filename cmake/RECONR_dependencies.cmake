a10f8a7137b447963aab52100f9fcd1671eea906
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2020-08-10 08:32:36 -0600

    Updating cmake dependency tag.

--- cmake/RECONR_dependencies.cmake
+++ cmake/RECONR_dependencies.cmake
@@ -27,7 +27,7 @@ FetchContent_Declare( constants
 
 FetchContent_Declare( resonanceReconstruction
     GIT_REPOSITORY  http://github.com/njoy/resonanceReconstruction
-    GIT_TAG         origin/feature/lrf7
+    GIT_TAG         origin/master
     )
 
 FetchContent_Declare( Log
