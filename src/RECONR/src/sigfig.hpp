8be101171f95c4a6a5f339d5059aab23f390f971
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2020-08-26 11:36:59 -0600

    Adding sigfig function.
    Ugh

---
+++ src/RECONR/src/sigfig.hpp
@@ -0,0 +1,14 @@
+/* sigfig will nudge a value up (or down) a given amount. 
+ *
+ * If the parameter, nudge, is positive, sigfig will nudge up.
+ * If the parameter, nudge, is negative, sigfig will nudge down.
+ *
+ * @param value: The value to be nudged.
+ * @param nudge: The relative fraction of value by which value will be nudged.
+ *
+ * I didn't want to write this, but it is just needed in so many places to be
+ * conformant with Legacy NJOY
+ */
+constexpr double sigfig( const double value, const double nudge = 1E-7 ){
+  return value*( 1 + nudge );
+}
